e83a371be86edec4b697d74b9d97163c75b38707
Eric Anderson <ericjanderson4@gmail.com>
2016-11-21 14:46:58 -0700

    added very hacked ability to inject html documents for tree

--- _plugins/enrichAngular.js
+++ _plugins/enrichAngular.js
@@ -1,4 +1,40 @@
 const fs = require('fs');
+var pjson = require('../../../package.json');
+var pversion=pjson.version;
+var pname=pjson.name;
+var output_path = './docs/'+pname+'/'+pversion+'/';
+var stub_path = output_path+'root.blank.js.html';
+var out_base_path = __dirname.substring(0,__dirname.indexOf("node_modules"));
+var out_remove_path = out_base_path+"src/app";
+var out_path = out_base_path+"docs/"+pname+'/'+pversion+'/';
+
+if (!String.prototype.encodeHTML) {
+  String.prototype.encodeHTML = function () {
+    return this.replace(/&/g, '&amp;')
+               .replace(/</g, '&lt;')
+               .replace(/>/g, '&gt;')
+               .replace(/"/g, '&quot;')
+               .replace(/'/g, '&apos;');
+  };
+}
+function encodeHTML(string){
+	return string.replace(/&/g, '&amp;')
+               .replace(/</g, '&lt;')
+               .replace(/>/g, '&gt;')
+               .replace(/"/g, '&quot;')
+               .replace(/'/g, '&apos;');
+}
+
+try{
+	fs.statSync(stub_path)
+}
+catch (e){
+	fs.closeSync(fs.openSync(stub_path, 'w'));
+}
+var stub = fs.readFileSync(output_path+'root.blank.js.html').toString();
+console.log(stub);
+var splitItem="/** @fileOverview  */"
+var index = stub.indexOf(splitItem)
 
 function getTagValue(tags,name){
     if(!tags) return null;
@@ -24,6 +60,24 @@ function enrichTemplate(item){
 	    var info=fs.readFileSync(path).toString();
 	    console.log(info);
 	    item.template = info;
+	    console.log("Place here",pname,pversion);
+		if(index>-1){
+			var new_stub = stub;
+			var replace_string = encodeHTML(info);
+			console.log("HEY THERE",index);
+			new_stub = new_stub.replace(splitItem,replace_string);
+			new_stub = new_stub.replace("sunlight-highlight-javascript","sunlight-highlight-xml");
+			new_stub = new_stub.replace("sunlight-highlighted","");
+			new_stub = new_stub.replace(/root\.blank\.js/g,name);
+			var relative_path = item.meta.path.replace(out_remove_path,"").replace(/^\//,"").replace('/','_').replace(/^_/,"");
+			console.log('rel',relative_path);
+			var use_path = out_path+relative_path+"_"+name+".html";
+			if(name=="root.html"){
+				use_path = out_path+relative_path+name+".html";
+			}
+			console.log(use_path);
+			fs.writeFileSync(use_path,new_stub);
+		}
 	}
 	
     }    
@@ -43,7 +97,7 @@ function enrichNgType(item){
 }
 
 exports.handlers = {
-    newDoclet: function(e) {
+    newDoclet: function(e) {	
 	e.doclet = enrichTemplate(e.doclet);
 	e.doclet = enrichNgType(e.doclet);
         // Do something when we see a new doclet
